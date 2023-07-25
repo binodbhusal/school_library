5f8d0aaac1ef54b4314e8bcdf6f36ef4def50b7a
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-07-25 16:01:48 +0100

    Add decorator to class person

--- person.rb
+++ person.rb
@@ -1,12 +1,46 @@
-class Person
+class Nameable
+  def correct_name
+    raise NotImplementedError
+  end
+end
+
+class Decorator < Nameable
+  def initialize(nameable)
+    @nameable = nameable
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
+
+class CapitalizeDecorator < Decorator
+  def correct_name
+    @nameable.correct_name.capitalize
+  end
+end
+
+class TrimmerDecorator < Decorator
+  def correct_name
+    original_name = @nameable.correct_name
+    original_name.length > 10 ? original_name[0..9] : original_name
+  end
+end
+
+class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age
 
   def initialize(id, age, name = 'Unknown', parent_permission: true)
+    super()
     @id = id
     @age = age
     @name = name
     @parent_permission = parent_permission
+  end
+
+  def correct_name
+    @name
   end
 
   def can_use_services?
@@ -19,3 +53,12 @@ class Person
     @age >= 18
   end
 end
+
+person = Person.new(1, 22, 'Maximilianus')
+puts person.correct_name # Output: Maximilianus
+
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name # Output: Maximilianus
+
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name # Output: Maximilian
