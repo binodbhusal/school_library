2332d56b0a27ef3312cd82d31cc0e3d30c5d954b
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-07-26 15:38:29 +0100

    update

--- classroom.rb
+++ classroom.rb
@@ -5,11 +5,11 @@ class Classroom
 
   def initialize(label)
     @label = label
-    @student = []
+    @students = []
   end
 
   def add_student(student)
-    @student << student
+    @students << student
     student.classroom = self
   end
 end
--- student.rb
+++ student.rb
@@ -6,9 +6,9 @@ class Student < Person
     @classroom = classroom
   end
 
-  def assign_classroom(classroom)
+  def classroom=(classroom)
     @classroom = classroom
-    classroom&.add_student(self)
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
